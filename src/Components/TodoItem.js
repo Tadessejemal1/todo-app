abc326322801c114a448c28bf8239e102e416137
Tadesse Jemal <tadessejemal63@gmail.com>
2022-09-12 19:56:40 +0300

    Add function for edditing items

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,31 +1,71 @@
+import React, {Component} from 'react';
+
 // css styles module
 import styles from "./TodoItem.module.css"
 
-function TodoItem(props) {
-  const completedStyle = {
-    fontStyle: "italic",
-    color: "#595959",
-    opacity: 0.4,
-    textDecoration: "line-through",
+class TodoItem extends Component {
+    state = {
+      editing: false,
+    }
+
+    handleEditing = () => {
+      this.setState({
+        editing: true,
+      })
+    }
+    
+    handleUpdatedDone = (event) => {
+      if (event.key === "Enter") {
+        this.setState({ editing: false })
+      }
+    }
+
+  render(){
+    let viewMode = {};
+    let editMode = {};
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+
+    const {completed, id, title } = this.props.todo;
+  
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)} />
+          <button onClick={() => this.props.deleteTodoProps(id)} >
+            Delete
+          </button>
+          <span style={completed ? completedStyle : null}>
+            {title}
+          </span>
+        </div>
+        <input 
+          type="text" 
+          style={editMode}
+          value={title}
+          className={styles.textInput} 
+          onChange={e => {
+            this.props.setUpdate(e.target.value, id)
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
+      </li>
+    )
   }
-
-  const {completed, id, title } = props.todo;
-
-  return (
-    <li className={styles.item}>
-      <input 
-      type="checkbox" 
-      className={styles.checkbox}
-      checked={completed} 
-      onChange={() => props.handleChangeProps(id)} />
-      <button onClick={() => props.deleteTodoProps(id)} >
-        Delete
-      </button>
-      <span style={completed ? completedStyle : null}>
-        {title}
-      </span>
-    </li>
-  )
 }
 
 export default TodoItem;
--- src/Components/TodosList.js
+++ src/Components/TodosList.js
@@ -8,10 +8,11 @@ class TodoList extends React.Component {
       <ul>
         {this.props.todos.map(todo => (
           <TodoItem 
-          key={todo.id} 
-          todo={todo} 
-          handleChangeProps={this.props.handleChangeProps}
-          deleteTodoProps={this.props.deleteTodoProps}
+            key={todo.id} 
+            todo={todo} 
+            handleChangeProps={this.props.handleChangeProps}
+            deleteTodoProps={this.props.deleteTodoProps}
+            setUpdate={this.props.setUpdate}
           />
         ))}
       </ul>
