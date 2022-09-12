fade6da98ced2ff5afdd5d48cc41cc06bf88e707
Tadesse Jemal <tadessejemal63@gmail.com>
2022-09-12 18:27:01 +0300

    Add events

--- src/Components/TodosList.js
+++ src/Components/TodosList.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
+
 import TodoItem from './TodoItem';
 
-class TodoList extends React.Component {
-  render() {
-    return (
+class TodosList extends React.Component {
+  render(){
+    return(
       <ul>
-        {this.props.todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
+        {this.props.todos.map(todo => (
+          <TodoItem 
+          key={todo.id} 
+          todo={todo} 
+          handleChangeProps={this.props.handleChangeProps}
+          deleteTodoProps={this.props.deleteTodoProps}
+          />
         ))}
       </ul>
-    );
-  }
+    )
+  } 
 }
 
-export default TodoList;
+export default TodosList;
